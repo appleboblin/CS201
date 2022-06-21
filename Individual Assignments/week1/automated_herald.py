bafb99695736408e9a1aa3ee51fb3c16c0f2bc09
appleboblin <appleboblin2430@gmail.com>
2022-06-20 23:14:52 -0700

    First assignment, hope I'm doing this right.

---
+++ Individual Assignments/week1/automated_herald.py
@@ -0,0 +1,1 @@
+print("I've set up a github repository, intend to upload all my work for this class into the repository. \nLink is: https://github.com/appleboblin/CS201 \nWondering if I can do that?")
